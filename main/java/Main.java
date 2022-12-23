e48ee5ac4833111a025a33701ba63428b15dcdcb
Daria Ruzanova <dashochek-28@mail.ru>
2022-12-23 10:13:29 +0400

    Init commit

---
+++ main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        BinOps binOps = new BinOps();
+        System.out.println(binOps.sum("101", "1000")); //5, 8
+        System.out.println(binOps.mult("101", "10")); //5,2
+    }
+}
